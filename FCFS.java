ba55065a4008b51db5fa979e4a18197fff462533
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2022-11-26 19:31:17 +0530

    fcfs added

--- FCFS.java
+++ FCFS.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class FCFS {
-    private Queue q = new Queue();
+    private PriorityQueue q = new PriorityQueue();
     ArrayList<Process> ganteChart = new ArrayList<>();
 
     public void add(Process p) {
@@ -9,9 +9,11 @@ public class FCFS {
     }
 
     public void run() {
-        int prevExitTime = q.top.data.arrivalTime;
+        int prevExitTime = 0;
         while (!q.isEmpty()) {
             Process currentProcess = q.dequeue();
+            if (prevExitTime < currentProcess.arrivalTime)
+                ganteChart.add(new Process("free", prevExitTime, currentProcess.arrivalTime));
             int extraFactor = (prevExitTime < currentProcess.arrivalTime) ? currentProcess.arrivalTime : prevExitTime;
             currentProcess.exitTime = currentProcess.burstTime + extraFactor;
             currentProcess.turnAroundTime = currentProcess.exitTime - currentProcess.arrivalTime;
@@ -25,9 +27,21 @@ public class FCFS {
     public void printChart() {
         System.out.println("\nPid\t AT\t BT\t ET\t TAT\t WT");
         for (Process p : ganteChart) {
+            if (p.id == "free")
+                continue;
             System.out.println(p.id + "\t " + p.arrivalTime + "\t " + p.burstTime + "\t "
                     + p.exitTime + "\t " + p.turnAroundTime + "\t " + p.waitTime
                     + "\t ");
         }
     }
+
+    public void ganteChart() {
+        System.out.println();
+        System.out.print("|");
+        for (Process p : ganteChart) {
+            System.out.print("  " + p.id + "  |");
+        }
+        System.out.println();
+    }
+
 }
--- User.java
+++ User.java
@@ -1,21 +1,32 @@
 import java.util.*;
 
 public class User {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        FirstComeFirstServe();
+    }
+
+    private static void FirstComeFirstServe() {
         FCFS fcfs = new FCFS();
-        int x = 5;
-        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of processes - ");
+        int processes = sc.nextInt();
         System.out.println("Enter pid, AT, BT");
-        while (x-- > 0) {
+        // int priority = 0;
+        while (processes-- > 0) {
             String pid = sc.next();
             int at = sc.nextInt();
             int bt = sc.nextInt();
             Process current = new Process(pid, at, bt);
+            current.priority = current.arrivalTime;
             fcfs.add(current);
+            // priority++;
         }
         fcfs.run();
         fcfs.printChart();
+        fcfs.ganteChart();
     }
+
 }
 
 /*
@@ -25,4 +36,17 @@ p1 1 3
 p2 2 1
 p3 3 2
 p4 4 5
+ * tc 2
+a 3 4
+b 5 3
+c 0 2
+d 5 1
+e 4 3
+*tc3
+p0 6 4
+p1 2 5
+p2 3 3
+p3 1 1
+p4 4 2
+p5 5 6
  */
